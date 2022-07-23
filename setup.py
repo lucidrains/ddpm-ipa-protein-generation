c81ad478b8b6e083695db4db6fd3e57723639878
Phil Wang <lucidrains@gmail.com>
2022-07-23 09:03:18 -0700

    include own library as dependency

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+setup(
+  name = 'ddpm-ipa-protein-generation',
+  packages = find_packages(exclude=[]),
+  version = '0.0.1',
+  license='MIT',
+  description = 'DDPM + Invariant Point Attention - Protein Generation',
+  author = 'Phil Wang',
+  author_email = 'lucidrains@gmail.com',
+  long_description_content_type = 'text/markdown',
+  url = 'https://github.com/lucidrains/ddpm-ipa-protein-generation',
+  keywords = [
+    'artificial intelligence',
+    'deep learning',
+    'attention mechanism',
+    'geometric deep learning',
+    'denoising diffusion probabilistic models'
+  ],
+  install_requires=[
+    'invariant-point-attention>=0.2.1',
+    'einops>=0.4',
+    'torch>=1.6',
+  ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'Intended Audience :: Developers',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
